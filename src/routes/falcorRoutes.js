8cd43f69935ac32d5cbf777de4cbe240c9ca0453
mitel <mitelone@gmail.com>
2015-11-07 10:43:33 +0300

    Add falcor routes

--- src/routes/falcorRoutes.js
+++ src/routes/falcorRoutes.js
@@ -2,7 +2,7 @@ import FalcorServer from 'falcor-hapi';
 import Router from 'falcor-router';
 import initSDK from '../avamar-sdk/initSDK';
 
-let session, resourcePool, dpr, tenant, resourceShare, folder; // eslint-disable-line
+// let session, resourcePool, dpr, tenant, resourceShare, folder; // eslint-disable-line
 const sdk = initSDK();
 
 module.exports = [
@@ -17,7 +17,96 @@ module.exports = [
           route: 'greeting',
           // respond with a PathValue with the value of "Hello World."
           get: () => {
-            return {path: ['greeting'], value: 'Hello World'};
+            return {path: ['greeting'], 
+            value: 'Hello World',
+          };
+          },
+        },
+        {
+          route: 'version',
+          async get() {
+            console.log('..call to version route');
+            let ver = {};
+            
+            try {
+              ver = await sdk.getVersion();
+            } catch (err) {
+              console.log(err);
+            }
+
+            return {
+              path: ['version'],
+              value: ver.versions,
+            };
+          },
+        },
+        {
+          route: 'authToken',
+          async get() {
+            console.log('.. call to get an auth token');
+            let session;
+
+            try {
+              session = await sdk.login();
+            } catch (err) {
+              console.log(err);
+              throw new Error(err);
+            }
+
+            return [
+              { path: ['authToken'], value: session.authToken},
+              { path: ['serviceProvider'], value: session.serviceProviderId },
+            ];
+          },
+        },
+
+        {
+          route: 'session.login',
+          async call(callPath, args) {
+            console.log('.. call to login ..');
+            let session;
+            const [ username, password ] = args;
+            
+            try {
+              session = await sdk.login(username, password);
+            } catch (err) {
+              console.log(err);
+              throw new Error(err);
+            } 
+
+            return [
+              { path: ['authToken'], value: session.authToken},
+              { path: ['serviceProvider'], value: session.serviceProviderId },
+            ];
+          },
+        }, 
+
+        // https://github.com/Netflix/falcor-router-demo/blob/master/index.js
+        {
+          route: 'resourcePool.create',
+          async call(callPath, args) {
+            console.log('.. call to create a resource pool');
+            let resourcePool;
+            const [ authToken, serviceProviderId, resourcePoolName] = args;
+            // console.log(args);
+
+            // prepare the parameter for the sdk call..
+            const resourcePoolConfig = {
+              name: resourcePoolName,
+            };
+
+            try {
+              // console.log(authToken);
+              resourcePool = await sdk.createResourcePool(authToken, serviceProviderId, resourcePoolConfig);
+            } catch (err) {
+              console.log(err);
+              throw new Error(err);
+            } 
+
+            return {
+              path: ['resourcePool'],
+              value: resourcePool.resourcePoolId,
+            };
           },
         },
       ]);
